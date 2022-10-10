9ec0e12368a82eceed961ab4915a59f94527af28
Paulo Meira <meira.paulo@outlook.com>
2022-10-10 19:11:20 -0300

    init project

---
+++ src/main/java/com/mdpaulo/commentit/CommentitApplication.java
@@ -0,0 +1,13 @@
+package com.mdpaulo.commentit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CommentitApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CommentitApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mdpaulo/commentit/CommentitApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mdpaulo.commentit;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CommentitApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
