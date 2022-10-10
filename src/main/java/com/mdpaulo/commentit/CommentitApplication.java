fe6cdd8481bcf9361c41995740be676c70cf9b8f
Paulo Meira <meira.paulo@outlook.com>
2022-10-10 19:14:54 -0300

    adding test for spring context

--- src/main/java/com/mdpaulo/commentit/CommentitApplication.java
+++ src/main/java/com/mdpaulo/commentit/CommentitApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class CommentitApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(CommentitApplication.class, args);
 	}
-
 }
--- src/test/java/com/mdpaulo/commentit/CommentitApplicationTests.java
+++ src/test/java/com/mdpaulo/commentit/CommentitApplicationTests.java
@@ -1,13 +1,15 @@
 package com.mdpaulo.commentit;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
 
 @SpringBootTest
 class CommentitApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext context) {
+		Assertions.assertNotNull(context);
 	}
-
 }
