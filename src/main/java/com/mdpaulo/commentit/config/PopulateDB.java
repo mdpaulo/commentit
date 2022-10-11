e13a19d53503142dc96122ba2147188aa5de74c1
Paulo Meira <meira.paulo@outlook.com>
2022-10-11 11:53:06 -0300

    Adding PopulateDB

---
+++ src/main/java/com/mdpaulo/commentit/config/PopulateDB.java
@@ -0,0 +1,28 @@
+package com.mdpaulo.commentit.config;
+
+import com.mdpaulo.commentit.domain.models.User;
+import com.mdpaulo.commentit.repository.UserRepository;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Configuration
+@Profile("!prod")
+public class PopulateDB implements CommandLineRunner {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Override
+    public void run(String... args) throws Exception {
+        userRepository.deleteAll();
+
+        User maria = new User(null, "Maria Brown", "maria@gmail.com");
+        User alex = new User(null, "Alex Green", "alex@gmail.com");
+        User bob = new User(null, "Bob Grey", "bob@gmail.com");
+
+        userRepository.saveAll(List.of(maria, alex, bob));
+    }
+}
